f77c60ac41eb532ba327760d534c5f5d61aa5107
Baba Dorin <bvd.dorin@gmail.com>
2021-01-12 14:40:11 +0200

    Introducere in OOP

---
+++ MedieDeConcurs/CalculareMedie/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculareMedie
+{
+    public class Class1
+    {
+        public double medieExamene(double m1, double m2, double m3, double m4s)
+        {
+            return (m1 + m2 + m3 + m4) / 4;
+        }
+    }
+}
---
+++ MedieDeConcurs/MedieDeConcurs/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MedieDeConcurs
+{
+    class Imagine
+    {
+        public string path;
+        public int dimensiuneX;
+        public int dimensiuneY;
+    }
+
+    class Automibil999
+    {
+        public string tip;
+        public string Nume;
+
+        public List<Imagine> imagini;
+
+        public string descriere;
+
+        public string Model;
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Automibil999 automobil = new Automibil999();
+            automobil.tip = "Automobil";
+            automobil.Nume = "BMW 5 Series";
+            automobil.descriere = "In stare buna";
+
+
+            Console.WriteLine(automobil.tip);
+            Console.WriteLine(automobil.Nume);
+            Console.WriteLine(automobil.descriere);
+        }
+    }
+}
