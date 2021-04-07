7f0144c57d57c74addfa1226e9113071c4a8b9a4
Baba Dorin <bvd.dorin@gmail.com>
2021-04-07 09:32:10 +0300

    l13 has been added

---
+++ Laborator13/Sarcina1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+namespace Sarcina1
+{
+    class CeaMaiSmecheraListaDeLaIisusHristosIncoace<T>
+    {
+        public List<T> Lista { get; set; } = new List<T>();
+
+        public void Add(T item)
+        {
+            if(Lista.Count == 0)
+            {
+                Lista.Add(item);
+                return;
+            }
+
+            int randomIndex = (new Random()).Next(0, Lista.Count);
+
+            List<T> lista1 = Lista.GetRange(0, randomIndex);
+            lista1.Add(item);
+            List<T> lista2 = Lista.GetRange(randomIndex, Lista.Count - randomIndex);
+            
+            Lista = lista1.Concat(lista2).ToList();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CeaMaiSmecheraListaDeLaIisusHristosIncoace<int> listaSmechera = new CeaMaiSmecheraListaDeLaIisusHristosIncoace<int>();
+            listaSmechera.Add(1);
+            listaSmechera.Add(2);
+
+            foreach (var item in listaSmechera.Lista)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
